f90439050e21cd7a0006f87d022426160e2b7273
jm <wlsauddlv@gmail.com>
2020-12-31 15:37:21 +0900

    모델생성

---
+++ src/com/cos/blog/service/Userservice.java
@@ -0,0 +1,24 @@
+package com.cos.blog.service;
+
+import com.cos.blog.domain.user.User;
+import com.cos.blog.domain.user.dto.JoinReqDto;
+import com.cos.blog.domain.user.dto.LoginReqDto;
+import com.cos.blog.domain.user.dto.UpdateReqDto;
+
+public class Userservice {
+	public int 회원가입(JoinReqDto dto) {
+		return -1;
+	}
+
+	public User 로그인(LoginReqDto dto) {
+		return null;
+	}
+
+	public int 회원수정(UpdateReqDto dto) {
+		return -1;
+	}
+
+	public int 아이디중복체크(String username) {
+		return -1;
+	}
+}
