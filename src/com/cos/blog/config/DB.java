4e25efa1656c1047a92f5982b7c06595630fcebd
jm <wlsauddlv@gmail.com>
2020-12-31 18:11:32 +0900

    필터적용및 DAO

---
+++ src/com/cos/blog/config/DB.java
@@ -0,0 +1,24 @@
+package com.cos.blog.config;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DB {
+	public static Connection getConnection() {
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/TestDB");
+			Connection conn = ds.getConnection();
+			System.out.println("디비 연결됨");
+			return conn;
+		} catch (Exception e) {
+			System.out.println("DB연결안됨");
+		}
+
+		return null;
+	}
+}
---
+++ src/com/cos/blog/domain/user/UserDao.java
@@ -0,0 +1,19 @@
+package com.cos.blog.domain.user;
+
+public class UserDao {
+	public void save() {
+
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void usernameCheck() {
+		
+	}
+	
+	public void findById() {
+		
+	}
+}
